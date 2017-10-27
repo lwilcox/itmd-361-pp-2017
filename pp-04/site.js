acaead7d72e9c5264da202a532ede224f89a7c8b
Logan Wilcox <lwilcox@hawk.iit.edu>
2017-10-27 13:24:16 -0500

    completed the production problem

--- pp-04/site.js
+++ pp-04/site.js
@@ -2,26 +2,9 @@
 //  outputs the correct value for x (5) even after double(6) is
 //  called:
 
-/*var x = 5;
-
-function double(num) {
-  var x = num * 2;
-  return x;
-}
-
-double(6);
-console.log('The value of x is:', x, 'It should be 5.'); */ //commented out for problem 2
-
-//  2. Rewrite the corrected JavaScript above as a self-executing
-//  anonymous function that does not pollute the global
-//  namespace (e.g., neither its variables nor the double
-//  function can be accessed via the `window` global object,
-//  like `window.x`):
-
+var x = 5;
 //self executing anonymous function
 (function() {
-  var x = 5;
-
   function double(num) {
     var x = num * 2;
     return x;
@@ -31,11 +14,20 @@ console.log('The value of x is:', x, 'It should be 5.'); */ //commented out for 
 
 })();
 
+//  2. Rewrite the corrected JavaScript above as a self-executing
+//  anonymous function that does not pollute the global
+//  namespace (e.g., neither its variables nor the double
+//  function can be accessed via the `window` global object,
+//  like `window.x`):
+
+
+
 
 //  3. Correct this function so that there is no i variable in
 //  the global scope:
 
 function arrayEach(array, func) {
+  var i = 0; //since i is now defined within the arrayEach function, i no longer has a global scope and is 'undefined' outside
   for (i = 0; i < array.length; i++) {
     func(array[i]);
   }
@@ -48,6 +40,11 @@ console.log(i) // should be 'undefined', not 3
 //  4. Explain why this function does not modify the global
 //  variable x declared on line 5 above. Write your explanation
 //  as JavaScript comments.
+
+//This function does not modify the global variable x because the function
+// seems to be making 'x' a local variable within the function. So when it
+// returns x, it is returning the local 'x' that is defined and calculated within
+// the addTwo function.
 
 function addTwo(x) {
   x = x + 2;
