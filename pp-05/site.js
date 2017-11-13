cc82e626420573a76ea97729651fb1ce3fe89d56
Logan Wilcox <lwilcox@hawk.iit.edu>
2017-11-12 18:02:37 -0600

    modified javascript file for event listener

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,4 +1,20 @@
+
+
+$(document).ready(function() {
+$("#doorbell-sound").remove();
+
 function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
 }
+
+$("#doorbell").on("click", function() {
+    ringDoorbell();
+});
+
+$(document).on("keypress", function(event) {
+    if(event.key === "d") {
+      ringDoorbell();
+    }
+  });
+});
